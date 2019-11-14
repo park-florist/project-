08e687084f35678939d90bad04524f7e8a74e445
UlaMhanna <mhana.ula@gmail.com>
2019-11-14 18:10:38 +0200

    Ola push1

--- FlowersShop copy 2/M3/server.js
+++ FlowersShop copy 2/M3/server.js
@@ -1,14 +1,23 @@
 const express = require("express");
 let app = express();
-// var httpProxy = require("http-proxy");
-// var Proxy1 = require("../proxy");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 app.use(express.static("client/dist"));
+app.use(bodyParser.json({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static("../public"));
 
+const Item = require("../database/data.js").Item;
+
+app.get("/items", (req, res) => {
+  console.log("i'm inside the get");
+  Item.find({})
+    .sort({ date: -1 })
+    .then(items => res.json(items), console.log("items inside the.Get server"));
+  // .then(items => res.json(items));
+});
 //var proxy = httpProxy.createProxyServer(options);
-let port = 4003;
+let port = process.env.PORT || 4003;
 app.listen(port, function() {
   console.log("hi from 4003");
   console.log("lestining from 4003");
---
+++ FlowersShop copy 2/proxy/index.jsx
@@ -0,0 +1,6 @@
+class Flowers7000 extends React.Component {
+  render() {
+    return <div>hi proxy jsx </div>;
+  }
+}
+ReactDOM.render(<Flowers7000 />, document.getElementById("Flowers7000"));
