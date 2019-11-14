de90dfdd0020c5373dfee867c559430c4500510f
NouranAd <54473203+NouranAd@users.noreply.github.com>
2019-11-14 18:15:53 +0200

    first try

--- FlowersShop copy 2/database/data.js
+++
@@ -1,7 +0,0 @@
-// const mongoose = require("mongoose");
-
-// mongoose.connect("mongodb://localhost/flowers");
-// let flowerSchema = mongoose.Schema({
-//   productInfo: { type: String, trim: true },
-//   productName: { type: String, trim: true, unique: true }
-// });
